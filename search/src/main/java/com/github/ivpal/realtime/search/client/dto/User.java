4f6c6f5dd2f6bac0abe7ff98fcd26b53982709cd
Pavel Ivanov <ivanvpvl@gmail.com>
2020-09-05 22:43:07 +0700

    Add router for user search

--- search/src/main/java/com/github/ivpal/realtime/search/client/dto/User.java
+++ search/src/main/java/com/github/ivpal/realtime/search/client/dto/User.java
@@ -1,4 +1,19 @@
 package com.github.ivpal.realtime.search.client.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class User {
+    private Long id;
+
+    @JsonProperty("first_name")
+    private String firstName;
+
+    @JsonProperty("last_name")
+    private String lastName;
 }
---
+++ search/src/main/java/com/github/ivpal/realtime/search/web/Router.java
@@ -0,0 +1,22 @@
+package com.github.ivpal.realtime.search.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+@Configuration
+@RequiredArgsConstructor
+public class Router {
+    private final SearchHandler searchHandler;
+
+    @Bean
+    RouterFunction<ServerResponse> routes() {
+        return route()
+                .GET("/search", searchHandler::searchUser)
+                .build();
+    }
+}
