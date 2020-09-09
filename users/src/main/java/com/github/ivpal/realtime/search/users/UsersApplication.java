f150f940ae7ec628c7b382ca12f22c756811c02d
Pavel Ivanov <ivanvpvl@gmail.com>
2020-09-09 17:23:03 +0700

    Change base path for UsersController

--- users/src/main/java/com/github/ivpal/realtime/search/users/UsersApplication.java
+++ users/src/main/java/com/github/ivpal/realtime/search/users/UsersApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CoreApplication {
+public class UsersApplication {
     public static void main(String[] args) {
-        SpringApplication.run(CoreApplication.class, args);
+        SpringApplication.run(UsersApplication.class, args);
     }
 }
--- users/src/main/java/com/github/ivpal/realtime/search/users/web/UsersController.java
+++ users/src/main/java/com/github/ivpal/realtime/search/users/web/UsersController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class UsersController {
     private final UserService userService;
